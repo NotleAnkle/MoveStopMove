d1f389f0ba5ae89dc29933a7d83856147a2ee66b
NotleAnkle <phamdacanh2k2@gmail.com>
2023-12-28 19:43:35 +0700

    + add pattern & package

---
+++ Assets/_Pattern/Cache.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Framework
+{
+    public static class Cache <T>
+    {
+        private static Dictionary<Collider, T> _dict = new Dictionary<Collider, T>();
+        public static T GetComponent(Collider collider)
+        {
+            if (_dict.TryGetValue(collider, out var value))
+            {
+                return value;
+            }
+            else
+            {
+                T collectItems = collider.GetComponent<T>();
+                _dict.Add(collider, collectItems);
+                return collectItems;
+            }
+        }
+            
+    }
+}
---
+++ Assets/_Pattern/Event/Scripts/EventID.cs
@@ -0,0 +1,13 @@
+﻿namespace _Framework.Event.Scripts
+{
+    public enum EventID
+    {
+        None = 0,
+        OnCharacterDie = 1,
+        OnPlayerHairChange = 2,
+        OnPlayerPantChange = 3,
+        OnPlayerWeaponChange = 4,
+        OnPlayerShieldChange = 5,
+        OnPlayerSetChange = 6,
+    }
+}
