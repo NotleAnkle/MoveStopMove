1c904b13e9dfa825bc4060b8c56fe05a93769627
NotleAnkle <phamdacanh2k2@gmail.com>
2023-12-28 19:40:36 +0700

    + add util scripts

---
+++ Assets/_Game/Scripts/Util/Constant.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constant
+{
+    public const string TAG_PLAYER = "Player";
+    public const string TAG_BOT = "Bot";
+
+    public const string ANIM_IDLE = "idle";
+    public const string ANIM_RUN = "run";
+    public const string ANIM_ATTACK = "attack";
+    public const string ANIM_DANCE_WIN = "win";
+    public const string ANIM_DEAD = "dead";
+    public const string ANIM_DANCE = "run";
+
+    public const float RANGE_DEFAULT = 8f;
+
+    public const float TIME_ATTACK_DELAY = 0.3f;
+}
